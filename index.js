4b42bf7445db49e1634865db1c2873bb790f2394
echadani_yassine <yassinechadani113@gmail.com>
2025-01-09 17:22:01 +0100

    Changes to be committed: 	modified:   index.js 	modified:   package.json 	modified:   vercel.json

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ const userRoutes = require('./routes/TrendifyAuthRoutes');
 const routerProduct = require('./routes/productsRoutes');
 const routerCategory = require('./routes/CategoriesRoutes');
 const routerAdmin = require('./routes/admin/adminRoutes');
-const orderRouter = require('./routes/OrderRoutes'); // Adjust path if necessary
+const orderRouter = require('./routes/OrderRoutes');
 const helmet = require('helmet');
-const cors = require('cors'); 
+const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require("./config/db");
 const paymentRoutes = require('./routes/paymentRoutes');
@@ -16,22 +16,19 @@ dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 4002;
-console.log('Server starting...');
 
 // Connect to MongoDB (use only one method)
 connectDB();
-
-
 
 // Security middleware
 app.use(helmet());
 app.use(express.json());
 app.use(cookieParser()); // For parsing cookies
 
-// Enable CORS
+// Enable CORS (consider removing this if defined in `vercel.json`)
 app.use(cors({
   origin: 'http://localhost:3000', // Allow requests from this origin
-  methods: ['GET', 'POST', 'PATCH' , 'PUT', 'DELETE'], // Allowed HTTP methods
+  methods: ['GET', 'POST', 'PATCH', 'PUT', 'DELETE'], // Allowed HTTP methods
   credentials: true // Allow cookies and credentials
 }));
 
@@ -43,7 +40,6 @@ app.use('/api/Trendify/Admin', routerAdmin);
 app.use('/api/Trendify/orders', orderRouter);
 app.use('/api/payments', paymentRoutes);
 app.use('/api/Trendify/feedback', feedbackRoutes);
-
 
 // Handle 404 errors
 app.use((req, res) => {
