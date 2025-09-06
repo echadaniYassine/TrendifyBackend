4c7a1e52aac1492c2b488cd5cd41a06357d6c88a
Yassine Echadani <yassinechadani113@gmail.com>
2025-09-06 12:54:24 +0100

    ..

--- services/PayPalPayment.js
+++ services/PayPalPayment.js
@@ -2,7 +2,7 @@ const paypal = require("paypal-rest-sdk");
 
 // Configure PayPal SDK
 paypal.configure({
-  mode: process.env.PAYPAL_MODE || "sandbox", // 'sandbox' for testing, 'live' for production
+  mode: process.env.PAYPAL_MODE || "sandbox", // 'sandbox' for testing, 'live' for production/
   client_id: process.env.PAYPAL_CLIENT_ID,
   client_secret: process.env.PAYPAL_SECRET,
 });
